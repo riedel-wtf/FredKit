ff7084145e417ccec0a7b49f4462144de20f9029
Frederik Riedel <frederik.riedel@frogg.io>
2020-11-08 09:10:41 +0100

    added new FredKit use cases: Subscription Manager, App Version Meta Data, Bulk TableViewCell Nib Registration, topViewController()

---
+++ Sources/FredKit/FredKitViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FredKitView.swift
+//  FredKit
+//
+//  Created by Frederik Riedel on 1/24/19.
+//  Copyright © 2019 Frederik Riedel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UIViewController {
+    var wrappedInNavigationController: UINavigationController {
+        return UINavigationController(rootViewController: self)
+    }
+    
+    static func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+        
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        
+        return base
+    }
+}
