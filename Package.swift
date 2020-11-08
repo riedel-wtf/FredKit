d2b8ca17f776a0de1430c8855b829cde62fe67cf
Frederik Riedel <frederik.riedel@frogg.io>
2020-11-08 09:55:21 +0100

    added dependency properly (hopefully?)

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FredKit",
-            dependencies: []),
+            dependencies: ["SwiftyStoreKit"]),
         .testTarget(
             name: "FredKitTests",
             dependencies: ["FredKit"]),
