04b6e73332d4f446153f26447a826959cab91c1a
Frederik Riedel <frederik@riedel.wtf>
2021-11-30 14:05:16 +0100

    fixed package dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FredKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/frogg/FredKitCharts.git", .branch("master"))
+        .package(url: "https://github.com/frogg/FredKitCharts.git", .upToNextMajor(from: "4.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
